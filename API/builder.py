6e28b6ec25355697a48ca1d9622ae3415c9e9e52
kacperbochan <kacperbochan@gmail.com>
2024-02-27 15:44:18 +0100

    builder building

--- API/builder.py
+++ API/builder.py
@@ -21,6 +21,29 @@ import asyncio
 router = APIRouter()
 templates = Jinja2Templates(directory="templates/project")
 
+async def translateToRenpy(scene: dict):
+    type = scene["type"].lower()
+    text = scene["text"].lower()
+    object_type = scene["object_type"].lower()
+    character = re.sub(cleanup, '',scene["character"]).lower()
+    location = re.sub(cleanup, '',scene["location"]).lower()
+    version = re.sub(cleanup, '',scene["version"]).lower()
+    action = scene["action"].lower()
+    place = scene["place"].lower()
+    
+    if(type == "dialogue"):
+        return f"{capital(character)} '{text}'"
+    
+    if(action == "show"):
+        if(object_type == "character"):
+            return f"show {character} {version} at {place} with dissolve"
+        else: 
+            return f"scene {location} {version} with fade"
+    
+    if(object_type == "character"):
+        return f"hide {character} {version} at {place} with dissolve"
+    return f"hide {location} {version} at {place} with fade"
+    
 def get_objects_without_images(project_path: str):
     characters= {}
     locations= {}
@@ -38,8 +61,16 @@ def get_objects_without_images(project_path: str):
     
     return characters, locations
 
-async def send_images(project_path: str, game_folder: str):
+cleanup = re.compile(r'[^a-zA-Z0-9]')
+
+# capitalize first letter of a string
+def capital(string):
+    return string[0].upper() + string[1:]
+
+async def create_definitions_and_images(project_path: str, game_folder: str):
     output_folder = os.path.join(game_folder, "game", "images")
+    
+    output_definitions = []
     
     output_images = []
     output_names = []
@@ -56,28 +87,51 @@ async def send_images(project_path: str, game_folder: str):
         locations = json.load(file)
     
     for character in characters.values():
+        output_definitions.append("define "+capital(re.sub(cleanup, '',character["name"]))+" = Character('"+capital(re.sub(cleanup, '',character["user_name"]))+"')")
         for version, values in character["versions"].items():
-            output_images.append(os.path.join(sprites_dir, values["image"]))
-            output_names.append(character["name"]+"_"+version+".png")
+            file_path = os.path.join(sprites_dir, values["image"])
+            if(file_path[-3:] == "png" and os.path.isfile(file_path)):
+                output_images.append(os.path.join(sprites_dir, values["image"]))
+                output_name = (re.sub(cleanup, '',character["name"])+"_"+re.sub(cleanup, '',version)+".png").lower()
+                output_names.append(output_name)
+                output_definitions.append("image "+re.sub(cleanup, '',character["name"]).lower()+" "+re.sub(cleanup, '',version).lower()+" = Image('"+output_name+"')")
+            else:
+                output_definitions.append("image "+re.sub(cleanup, '',character["name"]).lower()+" "+re.sub(cleanup, '',version).lower()+" =  Placeholder('boy', text = '"+re.sub(cleanup, '',character["name"]).lower()+" "+re.sub(cleanup, '',version).lower()+"')")
     
     for location in locations.values():
         for version, values in location["versions"].items():
-            output_images.append(os.path.join(locations_dir, values["image"]))
-            output_names.append(location["name"]+"_"+version+".png")
+            file_path = os.path.join(locations_dir, values["image"])
+            if(file_path[-3:] == "png" and os.path.isfile(file_path)):
+                output_images.append(os.path.join(locations_dir, values["image"]))
+                output_name = (re.sub(cleanup, '',location["name"])+"_"+re.sub(cleanup, '',version)+".png").lower()
+                output_names.append(output_name)
+                output_definitions.append("image "+re.sub(cleanup, '',location["name"]).lower()+" "+re.sub(cleanup, '',version).lower()+" = Image('"+output_name+"')")
+            else:
+                output_definitions.append("image "+re.sub(cleanup, '',location["name"]).lower()+" "+re.sub(cleanup, '',version).lower()+" =  Placeholder('bg', text = '"+re.sub(cleanup, '',location["name"]).lower()+" "+re.sub(cleanup, '',version).lower()+"')")
     
     for index, image in enumerate(output_images):
         if image[-3:] == "png":
             shutil.copy(image, output_folder +"\\" +output_names[index].lower())
     
-
-
+    return output_definitions
+    
 async def generate_game_script(project_path: str, game_folder: str):
-    return True
+    output_script = []
+    
+    scenes_file = os.path.join(project_path, "data", "build_data.json")
+    with open(scenes_file, 'r') as file:
+        scenes = json.load(file)
+    
+    output_script.append("label start:\n")
+    for scene in scenes:
+        output_script.append(f"    {await translateToRenpy(scene)}\n")
+    output_script.append("    return\n")
+    
+    return output_script
 
 async def launch_game(game_folder: str):
     run = os.path.join(game_folder, "Project.exe")
     subprocess.run([run])
-    
 
 async def replace_in_file(filename, new_value):
     with open(filename, 'r', encoding='utf-8') as file:
@@ -113,10 +167,20 @@ async def setup_game_folder(game_folder: str, project_name: str):
     renpy_exe_file = os.path.join(os.getcwd(), "renpy", "exe", "Project.exe")
     shutil.copy(renpy_exe_file, game_folder)
 
+async def create_game_files(game_folder: str, definitions: List[str], script: List[str]):
+    with open(os.path.join(game_folder, "game", "script.rpy"), 'w') as file:
+        file.write("\n".join(definitions))
+        file.write("\n")
+        for s in script:
+            file.write(s)
+            file.write("\n")
+
 async def generate_distribution(project_path: str, project_name: str):
     game_folder = await prepare_game_files(project_path, project_name, False)
-    await send_images(project_path, game_folder)
-    await generate_game_script(project_path, game_folder)
+    definitions = await create_definitions_and_images(project_path, game_folder)
+    script = await generate_game_script(project_path, game_folder)
+    create_game_files(game_folder, definitions, script)
+    
     
 async def prepare_game_files(project_path: str, project_name: str, testing: bool = True):
     game_folder = os.path.join(os.getcwd(),project_path, "game")
@@ -180,8 +244,9 @@ async def generate_game(request: Request, project_name: str):
     project, project_path = get_project_by_name(project_name)
     await prepare_game_files(project_path, project_name)
     game_folder = os.path.join(os.getcwd(),project_path, "game")
-    await send_images(project_path, game_folder)
-    
+    definitions =  await create_definitions_and_images(project_path, game_folder)
+    script = await generate_game_script(project_path, game_folder)
+    await create_game_files(game_folder, definitions, script)
     return {"status": "success"}
 
 @router.post("/start")
